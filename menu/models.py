3defd8d433b9f026f2dbf4e06bf8d733d44648ac
Brad <brad.pk@live.com>
2023-07-26 22:31:25 +0000

    g

--- menu/models.py
+++ menu/models.py
@@ -7,9 +7,24 @@ class TestTable(models.Model):
 
 
 class MenuItems(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    description = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    price = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    #fktocategory = models.CharField(max_length=200, null=False, blank=False, unique=True)  <--- WTF is FK to Category
+    #fktocuisine = models.CharField(max_length=200, null=False, blank=False, unique=True)   <--- WTF is FK to Cuisine
+     
 
 
 class Category(models.Model):
+    appetizer = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    dessert = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    maindish = models.CharField(max_length=200, null=False, blank=False, unique=True)
+# What is this and what else is needed
 
 
 class Cuisine(models.Model):
+        american = models.CharField(max_length=200, null=False, blank=False, unique=True)
+        thai = models.CharField(max_length=200, null=False, blank=False, unique=True)
+# What is this and what else is needed
+
+
