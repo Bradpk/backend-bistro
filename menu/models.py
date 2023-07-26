210b826a25ea4ecffa37985a17279dff4a067e2e
Brad <brad.pk@live.com>
2023-07-26 22:25:42 +0000

    g

--- menu/models.py
+++ menu/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class TestTable(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False, unique=True)
+
+
+class MenuItems(models.Model):
+
+
+class Category(models.Model):
+
+
+class Cuisine(models.Model):
